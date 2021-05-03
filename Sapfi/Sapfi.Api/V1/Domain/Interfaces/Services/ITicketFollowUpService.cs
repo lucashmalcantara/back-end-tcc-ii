6ae4a1bdafbdb3e9f8d3029dcf655ae28ba18e10
Lucas Alcântara <lucashmalcantara@gmail.com>
2021-05-02 23:34:01 -0300

    Implementa endpoint para exclusão do acompanhamento do ticket. (#28)
    * Implementa endpoint para exclusão do acompanhamento do ticket.
    
    * Corrige exclusão do acompanhamento do ticket.

--- Sapfi/Sapfi.Api/V1/Domain/Interfaces/Services/ITicketFollowUpService.cs
+++ Sapfi/Sapfi.Api/V1/Domain/Interfaces/Services/ITicketFollowUpService.cs
@@ -7,5 +7,6 @@ namespace Sapfi.Api.V1.Domain.Interfaces.Services
     public interface ITicketFollowUpService
     {
         Task<SimpleResult> Create(TicketFollowUp ticketFollowUp);
+        Task<SimpleResult> Delete(long ticketId, string deviceToken);
     }
 }
